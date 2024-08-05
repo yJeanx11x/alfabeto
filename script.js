67d185f0b73b74a97a1a36274f7ad483c8126d00
Jean Lucas <j3anlucas010@gmail.com>
2024-08-05 09:07:09 -0300

    Escolha Um Numero é a letra aparce

---
+++ script.js
@@ -0,0 +1,6 @@
+const alfabeto=[,'a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','z','x','y','z']
+
+const escolhanumeo=10
+const letralfa=alfabeto[escolhanumeo]
+if(escolhanumeo >=28) console.log('Escolha O número entre 1 a 27')
+else console.log(letralfa)
